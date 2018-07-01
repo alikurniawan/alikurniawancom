6bdc990cffe8377579b90fb0342d953ba327aab8
alikurniawan <alikoerniawan02@gmail.com>
2018-07-01 08:27:43 +0700

    New

--- js/meteam.js
+++ js/meteam.js
@@ -10,7 +10,7 @@ let getData = () => {
         followerList += `
         <div class="col-sm-6 col-md-4 mb-2 animated wobble">
         <div id="cardList" class="card" style="width: 18rem;">
-          <img class="card-img-top sizeAvatar circle" src='${data.avatar_url}' alt="Card image cap">
+          <img class="card-img-top mx-auto rounded-circle" src='${data.avatar_url}' alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title"><label>Name :${data.login}</label></h5>
             <p class="card-text">${data.bio}</p>
