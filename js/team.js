e4c7b79ed15745e5468cd59da0c57473fb362f8a
alikurniawan <alikoerniawan02@gmail.com>
2018-07-01 22:23:02 +0700

    Gitteam

--- js/team.js
+++ js/team.js
@@ -9,9 +9,32 @@ let getData = () => {
       datas.map((data, index) => {
         followerList += `
 
-            <div class="col-sm flexname">
-              <img class="card-img-top mx-auto rounded-circle teamsize" src='${data.avatar_url}' alt="Card image cap">
+
+
+            <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="${data.avatar_url}" alt="">
+              <h4>Kay Garland</h4>
+              <p class="text-muted">Fullstack Developer</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
+          </div>
 
         `;
       })
